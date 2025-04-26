bc9caf4282a29f8f80411dbb9859e0ac7607be98
nyr <>
2025-04-26 20:42:38 +0200

    Added basic interaction with showdown

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import subprocess
 
+POKEMON_SHOWDOWN = "./pokemon-showdown/pokemon-showdown"
+
 print("Hello World!")
+subprocess.run(args=[POKEMON_SHOWDOWN, '-h'])
---
+++ rebuild.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ./pokemon-showdown/
+./build
